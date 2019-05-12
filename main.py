57dfada1fb7a51e94022da4905e93ba7129592ca
Vasja Volin <vasja.volin@gmail.com>
2019-05-12 19:34:48 -0400

    try adding some prints and fix some minor issues

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ RANDOM_ENDPOINT = 'cards/random'
 DEFAULT_PARAMS = 'format=json'
 
 resp = requests.get("{}/{}?{}".format(SCRYFALL_BASE_URL, RANDOM_ENDPOINT, DEFAULT_PARAMS))
+print("made request to scryfall")
 # parse the json response and set up the card and gatherer images
 
 response = resp.json()
@@ -22,10 +23,12 @@ git_command = "git rev-list --no-merges HEAD ^master --pretty=oneline --abbrev-c
 with Popen([git_command], stdout=PIPE, shell=True) as proc:
   git_output = proc.stdout.read()
 
+print("grabbed git commits")
+
 lines = git_output.decode('utf-8').strip().split("\n")
 rev_list_regex = re.compile(r'^\w+ ')
 commit_messages = [re.sub(rev_list_regex, '', line) for line in lines]
-commit_lines = ["* {}".format(message) for message in commit_messages].join("\n")
+commit_lines = "\n".join(["* {}".format(message) for message in commit_messages])
 first_commit = commit_messages[0]
 
 # collate these into the pull request body
@@ -44,7 +47,16 @@ body = """
            gatherer_url=gatherer_url, card_name=card_name,
            commit_lines=commit_lines)
 
+print("built commit message")
+print("------")
 print(body)
+print("------")
+
 
 # use hub with this message to open a pull request
 
+hub_command = "hub pull-request -m {} -o -c -d".format(body)
+proc = Popen([hub_command], stdout=PIPE, stderr=PIPE, shell=True)
+proc.communicate()
+
+print("pushed it")
