7b5bdf35bf77864f18a363a1869ee034523664e8
Vasja Volin <vasja.volin@gmail.com>
2019-05-12 18:52:53 -0400

    add some pseudocode for this shit

---
+++ main.py
@@ -0,0 +1,5 @@
+# make a request to the scryfall API
+# parse the json response and set up the card and gatherer images
+# find the commits on your current branch since master
+# collate these into the pull request body
+# use hub with this message to open a pull request
