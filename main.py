8f37e6406003fb22978c7bfaca0d0e7baa0c53aa
Vasja Volin <vasja.volin@gmail.com>
2019-05-12 19:02:25 -0400

    start fetching images

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import system
+import requests
+
 # make a request to the scryfall API
+
+SCRYFALL_BASE_URL = 'https://api.scryfall.com'
+RANDOM_ENDPOINT = 'cards/random'
+DEFAULT_PARAMS = 'format=json'
+
+resp = requests.get("{}/{}?{}".format(SCRYFALL_BASE_URL, RANDOM_ENDPOINT, DEFAULT_PARAMS))
 # parse the json response and set up the card and gatherer images
+
+response = resp.json()
+card_art_url = response["image_uris"]["art_crop"]
+gatherer_url = response["related_uris"]["gatherer"]
+
 # find the commits on your current branch since master
+
+
+
 # collate these into the pull request body
 # use hub with this message to open a pull request
+
